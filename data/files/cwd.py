e17d8bb2c032922816f4d36a002b6a9bd282c88e
Janet <5odemj91@solent.ac.uk>
2021-03-24 21:04:05 +0100

    Modified Added code to display information about the current working directory.

--- data/files/cwd.py
+++ data/files/cwd.py
@@ -1,7 +1,6 @@
-# Display information about the current working directory
 import os
 
-# Retrieve the file path and display each file contained in the current working folder
+
 def cwd():
     path = os.getcwd()
     print(f"The current working directory is {path}")
@@ -9,8 +8,10 @@ def cwd():
     for file in os.listdir(path):
         print(file)
 
+
 def run():
     print("Processing...")
     cwd()
+
 
 run()
