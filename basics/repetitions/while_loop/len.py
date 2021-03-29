32ee7ec38b6ba233961a3243dd83ca0d720dbbfd
Janet <5odemj91@solent.ac.uk>
2021-03-29 15:34:28 +0100

    Modified codes.

--- basics/repetitions/while_loop/len.py
+++ basics/repetitions/while_loop/len.py
@@ -1,10 +1,4 @@
 # display the message
 print("Please enter a phrase:")
 phrase = input()
-
-numb_chars = len(phrase)
-Bop_displayed = 0
-print()
-while Bop_displayed < numb_chars:
-    print("Bop ", end="")
-    Bop_displayed = Bop_displayed + 1
+print(f"\n{'Bop '* len(phrase)}")
