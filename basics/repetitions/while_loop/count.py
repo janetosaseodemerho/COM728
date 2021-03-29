65f5ba608340dde90b414647c14ebd1a78d4c939
Janet <5odemj91@solent.ac.uk>
2021-03-29 15:34:03 +0100

    Modified codes.

--- basics/repetitions/while_loop/count.py
+++ basics/repetitions/while_loop/count.py
@@ -4,8 +4,9 @@ numb_cables = int(input())
 removed_cable = 0
 print()
 while removed_cable < numb_cables:
-    print(f"Avoiding...Done! {removed_cable + 1} live cables avoided.")
+    print("Avoiding...", end="")
     removed_cable += 1
+    print(f"Done! {removed_cable} live cables avoided.")
 
 # finally display message
 print("\nAll live cables have been avoided.")
