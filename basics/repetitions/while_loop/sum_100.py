96374cad56979df15f76fcbd7978060b47ef43bd
Janet <5odemj91@solent.ac.uk>
2021-03-29 15:34:52 +0100

    Modified codes.

--- basics/repetitions/while_loop/sum_100.py
+++ basics/repetitions/while_loop/sum_100.py
@@ -2,12 +2,11 @@
 print("Calculating the sum of the first 100 numbers...")
 
 number = 1
-
 total = 0
 
 while number <= 100:
     total = total + number
-    number = number + 1
+    number += 1
 
 # Display result
 print(f"...Done! The answer is {total}")
