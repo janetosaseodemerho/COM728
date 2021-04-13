17a1f79c17d56dababe2831c7ad616a584c8b839
Janet <5odemj91@solent.ac.uk>
2021-04-13 13:02:40 +0100

    Modified code to demonstrate the use of multiple function calls.

--- basics/functions/multiple_functions.py
+++ basics/functions/multiple_functions.py
@@ -7,9 +7,11 @@ def display_ladder(steps):
     # Display bottom of ladder
     print("| |")
 
+
 def create_ladder():
     print("How many steps remain?")
     steps = int(input())
     display_ladder(steps)
+
 
 create_ladder()
